f52bddb1dfca392857213195f5c21a8c9e0231f1
Prisha Jhajj <jhajjpr@sheridancollege.ca>
2023-02-03 23:46:22 -0500

    Added two import statements. The first import statement imports System.out. The second one imports Random.

--- src/main/java/sheridan/jhajjpr/assignment1/Main.java
+++ src/main/java/sheridan/jhajjpr/assignment1/Main.java
@@ -1,8 +1,14 @@
 package sheridan.jhajjpr.assignment1;
 
+import static java.lang.System.out;
+import java.util.Random;
+
+
 public class Main {
 
     public static void main(String[] args) {
+
+
 
     }
 
