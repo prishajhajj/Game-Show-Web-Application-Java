12ea42bd4dd75b0b6d5a46f0bd5dd3758ba97bd0
Prisha Jhajj <jhajjpr@sheridancollege.ca>
2023-02-04 21:26:18 -0500

    Added three constant variables playeroneChoice, playertwoChoice, playerthreeChoice.

---
+++ src/main/java/sheridan/jhajjpr/assignment1/model/PrizeSelection.java
@@ -0,0 +1,15 @@
+package sheridan.jhajjpr.assignment1.model;
+
+public class PrizeSelection {
+
+    private final PlayerChoice playeroneChoice;
+
+    private final PlayerChoice playertwoChoice;
+
+    private final PlayerChoice playerthreeChoice;
+
+    }
+
+
+
+
