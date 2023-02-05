2964487d5e045154d62ff33d455b09f7bf885bda
Prisha Jhajj <jhajjpr@sheridancollege.ca>
2023-02-04 22:26:28 -0500

    Added switch statements to randomize the prizes that come from each box.

--- src/main/java/sheridan/jhajjpr/assignment1/model/PrizeSelection.java
+++ src/main/java/sheridan/jhajjpr/assignment1/model/PrizeSelection.java
@@ -8,6 +8,8 @@ public class PrizeSelection {
 
     private final PlayerChoice playerthreeChoice;
 
+    private final GameResult gameResult;
+
     public PlayerChoice getPlayeroneChoice() {
         return playeroneChoice;
     }
@@ -19,6 +21,46 @@ public class PrizeSelection {
     public PlayerChoice getPlayerthreeChoice() {
         return playerthreeChoice;
     }
+
+    public GameResult getGameResult( ) {
+        return gameResult;
+    }
+
+    private static GameResult getGameResult(PlayerChoice playeroneChoice, PlayerChoice playertwoChoice, PlayerChoice playerthreeChoice) {
+        switch(playeroneChoice) {
+            case BOX1:
+                return GameResult.TICKETS;
+
+            case BOX2:
+                return GameResult.CANDY;
+
+            case BOX3:
+                return GameResult.CHEQUE;
+        }
+        switch(playertwoChoice) {
+            case BOX1:
+                return GameResult.CANDY;
+
+            case BOX2:
+                return GameResult.CHEQUE;
+
+            case BOX3:
+                return GameResult.TICKETS;
+        }
+        switch(playerthreeChoice) {
+            case BOX1:
+                return GameResult.CHEQUE;
+
+            case BOX2:
+                return GameResult.TICKETS;
+
+            case BOX3:
+                return GameResult.CANDY;
+        }
+        throw new AssertionError("should never be reached");
+    }
+
+
 
     }
 
