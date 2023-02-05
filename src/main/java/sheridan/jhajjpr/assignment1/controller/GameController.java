fd4ad34a964e92ebdc88eb6496033b3fd662e0f5
Prisha Jhajj <jhajjpr@sheridancollege.ca>
2023-02-04 22:40:18 -0500

    Added a GameController package, containing the GameController class.

---
+++ src/main/java/sheridan/jhajjpr/assignment1/controller/GameController.java
@@ -0,0 +1,16 @@
+package sheridan.jhajjpr.assignment1.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import sheridan.jhajjpr.assignment1.model.Main;
+
+import java.util.List;
+
+@Controller
+public class GameController {
+
+    //@RequestMapping(value="Result.html")
+    //public List<String> revealPrize( ) {
+    //return prizeslist;
+    //}
+}
