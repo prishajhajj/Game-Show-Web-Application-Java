086f8de1000726fe4dddb9c6a41f7c2d9fe26f30
Prisha Jhajj <jhajjpr@sheridancollege.ca>
2023-02-04 21:00:37 -0500

    Added @Controller on top of PlayGame class and @RequestMapping on top of revealPrize method.

--- src/main/java/sheridan/jhajjpr/assignment1/PlayGame.java
+++ src/main/java/sheridan/jhajjpr/assignment1/PlayGame.java
@@ -1,12 +1,13 @@
 package sheridan.jhajjpr.assignment1;
 
+import sheridan.jhajjpr.assignment1.Main;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import static java.lang.System.out;
 import java.util.List;
 import java.util.Random;
 
-
+//@Controller
 public class PlayGame {
 
     private final Random random;
@@ -14,5 +15,10 @@ public class PlayGame {
     public PlayGame(Random random) {
         this.random = random;
     }
+
+    //@RequestMapping(value="Result.html")
+    //public List<String> revealPrize( ) {
+        //return prizeslist;
+    //}
 
 }
