afd6089530b63b775678691eed72121d8dce1a4f
Prisha Jhajj <jhajjpr@sheridancollege.ca>
2023-02-04 20:49:12 -0500

    Deleted the boxValue variable, getValue(getter) and setValue(setter) methods, and deleted the valueGenerator and result methods.

--- src/main/java/sheridan/jhajjpr/assignment1/PlayGame.java
+++ src/main/java/sheridan/jhajjpr/assignment1/PlayGame.java
@@ -1,48 +1,18 @@
 package sheridan.jhajjpr.assignment1;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 import static java.lang.System.out;
+import java.util.List;
 import java.util.Random;
+
 
 public class PlayGame {
 
     private final Random random;
-    private int boxValue;
 
     public PlayGame(Random random) {
         this.random = random;
     }
-
-    public int getValue( ) {
-        return boxValue;
-    }
-
-    public final void setValue(int value) {
-        if(value >= 1 && value <= 3) {
-            this.boxValue = boxValue;
-        }
-        else {
-            throw new IllegalArgumentException("Number of Boxes is not valid" + boxValue);
-        }
-    }
-
-    public int valueGenerator( ) {
-        boxValue = 1 + (int)(3*Math.random());
-        return boxValue;
-    }
-
-    public int result( ) {
-        if(boxValue == 1) {
-            out.println("User chose Box 1");
-        }
-        if(boxValue == 2) {
-            out.println("User chose Box 2");
-        }
-        if(boxValue == 3) {
-            out.println("User chose Box 3");
-        }
-        return boxValue;
-    }
-
-
 
 }
