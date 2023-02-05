6ff61426c6c6e241c3149ac5e8c99f586c4b16f8
Prisha Jhajj <jhajjpr@sheridancollege.ca>
2023-02-04 20:44:06 -0500

    Added a list of prizes, called prizesList.

--- src/main/java/sheridan/jhajjpr/assignment1/Main.java
+++ src/main/java/sheridan/jhajjpr/assignment1/Main.java
@@ -1,19 +1,37 @@
 package sheridan.jhajjpr.assignment1;
 
 import static java.lang.System.out;
+import static java.util.Collection.*;
+
 import java.util.Random;
+import java.util.*;
+import java.util.stream.Collectors;
+
+import com.fasterxml.jackson.databind.introspect.VisibilityChecker;
+import sheridan.jhajjpr.assignment1.PrizeSelection;
 
 
 public class Main {
-
     public static void main(String[] args) {
 
         PlayGame obj = new PlayGame(new Random());
 
-        out.println("random number: " + obj.valueGenerator());
-        out.println(obj.result());
+        String[] prizes = {"Two Tickets to Florida", "KITKAT Chocolate Bar", "$500 Cheque"};
+        List<String> prizeslist = Arrays.asList((prizes));
+        System.out.println("Prize list");
+
+        //System.out.println("Shuffled Prize list");
+
+        for (String val : prizeslist) {
+            System.out.println(val + " ");
+        }
+
+        Collections.shuffle(prizeslist);
+        System.out.println(prizeslist);
+
+        //obj.result2();
+
 
 
     }
-
 }
